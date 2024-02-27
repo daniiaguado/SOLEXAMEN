483f8ac58f3c26ae84a402c0189014e9fe57ddf4
Daniel Aguado <daagro@floridauniversitaria.es>
2024-02-27 10:17:36 +0100

    1

---
+++ 1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+total=0
+
+while read -r linea;do
+
+sitio=$(echo "$linea "| awk '{print $1}')
+
+
+consumo=$(echo "$linea" | awk '{print $4}')
+
+if [ $1 = $sitio ]; then
+
+    total=$((total+consumo))
+
+fi
+
+done <consumos.txt
+
+echo "El consumo de $1 $total"
